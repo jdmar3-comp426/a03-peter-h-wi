13fdb291c640455dfda101a35775db3af0fcd241
Peter Wi <peter.h.wi96@gmail.com>
2021-10-12 17:45:32 -0400

    half way done

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,7 +15,11 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-    
+    let arr = []
+    for (let i = 0; i < n; ++i) {
+        arr.push(fn(...params));
+    }
+    return arr;
 };
 
 
@@ -24,7 +28,7 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-
+    repeat(console.log, 10, "Hello, world!")
 };
 
 
@@ -41,15 +45,17 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    const func2 = (num2) => {
+        return num1 * num2;
+    }
+    return func2;
 };
-
 
 /**
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -57,7 +63,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
@@ -85,7 +91,15 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    for (let i=0; i < arr.length; ++i) {
+        if (i%2!=0) {
+            continue;
+        }
+        if (!test(arr[i])) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
