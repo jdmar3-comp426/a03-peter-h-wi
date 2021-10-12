9ce40d30de84792dc9fe07b1c6c069e77b2e46c6
Peter Wi <peter.h.wi96@gmail.com>
2021-10-12 14:16:26 -0400

    mild2 done

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,12 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   let dict = new object();
+   dict = {
+      type = typeof(variable),
+      value = variable
+   };
+   return dict;
 }
 
 
@@ -24,7 +29,11 @@ export function identifyVariable(variable) {
 
  */
 export function identifyArray(array) {
-
+   let arr = []
+   array.forEach(element => {
+      arr.push(identifyVariable(element));
+   });
+   return arr;
 }
 
 /**
@@ -44,7 +53,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object[key];
 }
 
 /**
@@ -64,7 +73,8 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   let {key: omit, ...res} = object;
+   return res;
 }
 
 /**
@@ -89,5 +99,6 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   let {keyList: omit, ...res} = object;
+   return res;
 }
